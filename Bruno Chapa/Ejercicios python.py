5096eda6446d8a2a2e71ef5fb1b611e0c568eeb8
Bruno885-ops <haterxd85@gmail.com>
2025-04-29 07:02:02 -0300

    Add files via upload

---
+++ Bruno Chapa/Ejercicios python.py
@@ -0,0 +1,60 @@
+import math
+
+"""
+###Ejercicio 1
+
+V = int(input("Ingrese la velocidad en kilometros  "))
+T = int(input("Ingrese el tiempo en horas"))
+D = V* T
+print("el valor de la distancia recorrida es ", D)
+
+
+N= int(input("Ingrese la 1er nota parcial"))
+Ni = int(input("Ingrese la 2da nota parcial"))
+No = int(input("Ingrese la 3er nota parcial"))
+
+promedio = (N+Ni+No) / 3 
+print("El promedio de las notas es ", promedio)
+
+
+rc = int(input("Ingrese la cantidad de respuestas correctas"))
+ri = int(input("Ingrese la cantidad de respuestas incorrectas"))
+rb = int(input("Ingrese la cantidad de respuestas en blanco"))
+
+prc = rc * 3
+pri = ri * -1
+prb = rb * 0
+
+PF = pri+prc+prb
+print("El puntaje final es ", PF)
+
+
+pg = int(input("Ingrese la cantidad de partidos ganados"))
+pe = int(input("Ingrese la cantidad de partidos empatados"))
+pp = int(input("Ingrese la cantidad de partidos perdidos"))
+ppg = pg * 3
+ppe = pe * 1
+ppp = pp * 0
+
+pt= ppg+ppe+ppp
+print("El puntaje total es ", pt)
+
+
+gb = float(input("Ingrese la cantidad de gigabytes"))
+mg = gb*1024
+md = mg /1.44
+print(md)
+print("numero de discos necesarios : ", math.ceil(md))
+
+
+
+pg = int(input("Ingrese la cantidad de partidos ganados"))
+
+
+"""
+
+
+
+
+
+
