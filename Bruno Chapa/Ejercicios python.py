b6d42fceb9963cae79b59e507942109c39f47a09
Bruno885-ops <haterxd85@gmail.com>
2025-04-29 07:23:06 -0300

    Ejercicios python.py

--- Bruno Chapa/Ejercicios python.py
+++ Bruno Chapa/Ejercicios python.py
@@ -46,11 +46,30 @@ md = mg /1.44
 print(md)
 print("numero de discos necesarios : ", math.ceil(md))
 
+AX = float(input("Ingrese la coordenada X de A :"))
+AY = float(input("Ingrese la Coordenada Y de A :"))
+BX = float(input("Ingrese la Coordenada X de B :"))
+BY = float(input("Ingrese la Coordenada Y de B :"))
+
+D = ( (AX-BX)**2 + (AY-BY)**2 )**0.5
+
+print("Resultado", D)
 
 
-pg = int(input("Ingrese la cantidad de partidos ganados"))
 
 
+N = float(input("Ingrtese un primer numero"))
+NA = float(input("Ingrese un segundo numero"))
+S = N +NA
+P = S/2
+print("promedio",P)
+
+
+B = float(input("Ingrese base"))
+A = float(input("Ingrese alto"))
+s = B* A
+P = 2*(B+A)
+print("la superficie es ", s ,"y el perimetro es ", P)
 """
 
 
