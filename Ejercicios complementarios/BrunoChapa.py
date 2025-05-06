d1a9b811bac4d59f889845803b1928b0adbef96a
Bruno885-ops <haterxd85@gmail.com>
2025-05-06 06:48:23 -0300

    Add files via upload

---
+++ Ejercicios complementarios/BrunoChapa.py
@@ -0,0 +1,35 @@
+Pi = 3.1416
+
+R = float( input("Ingrese Radio: "))
+
+v = 4/3 * Pi * R**3
+
+
+
+print("El volumen de la esfera es:", v)
+
+a = float(input("Ingrese la altura del triangulo: "))
+
+b = 3*(2*a)/3**0,5
+
+print("El perimetro del triangulo es:",b)
+
+
+EU= 3.84
+DO = 2.28
+
+a = float(input("Ingrese la cantidad de soles:"))
+d = a / DO
+e = a / EU
+
+print("En", a, "soles hay ", e, "euros")
+print("En", a, "soles hay ", d, "dólares")
+
+a = float(input("Ingrese el tiempo en segundos:"))
+b = float(input("Ingrese la distancia en metros:"))
+v = b / a
+
+print("La velocidad es:", v ,"m/s")
+          
+
+
